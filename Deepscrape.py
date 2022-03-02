3a557824413a94afcf7efdacf52c33fa44c4380a
Vidit Dhull <vidit.dh@gmail.com>
2022-03-02 12:19:38 +0530

    Version 3

--- Deepscrape.py
+++ Deepscrape.py
@@ -2,4 +2,31 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
+output = 'Scraped_file'+'.json'
+result = []
+
+url = 'https://www.snapdeal.com/products/books?sort=plrty&SRPID=customsearch&keywd=books'
+file = requests.get(url)
+data = file.text
 soup = BeautifulSoup(data, 'html.parser')
+
+
+titles = soup.find_all('p', {'class' : 'product-title'})
+title = []
+for t in titles:
+    title.append(t.text)
+result.append({'titles':title})
+
+authors = soup.find_all('p', {'class' : 'product-author-name'})
+author = []
+for i in authors:
+    author.append(i.text)
+result.append({'authors': author})    
+
+prices = soup.find_all('span', {'class' : 'lfloat product-price'})
+price = []
+for i in prices:
+    price.append(i.text)
+result.append({'prices' : price})
+
+print(result)
